6bd0a8001c99140252cc6b7ceea2a6bc241f6d0f
arya237 <naderiarya20@gmail.com>
2024-03-24 21:48:36 +0330

    adjasency list completed

--- tehran.py
+++ tehran.py
@@ -1,8 +1,69 @@
 from collections import defaultdict as mymap
 from vehicle import vehicle
 
-# class Tehran:
-#     def __init__(self) -> None:
-#         self.graph = mymap(lambda: mymap(list))
+class Tehran:
+
+    def __init__(self) -> None:
+        self.graph = mymap(lambda: mymap(list))
+        self.read_from_file()
         
-#     def
+    def read_from_file(self) -> None:
+        start = ""
+        destination = ""
+        line = ""
+        value: int
+
+        file = open("taxi_bus_distance.txt", "r")
+        
+        line = file.readline().strip('\n')
+
+        while True:
+            start = file.readline().rstrip('\n')
+
+            if start == 'end*':
+                break
+
+            if start == 'end':
+               line = file.readline().strip('\n')
+               start = file.readline().rstrip('\n')
+
+            destination = file.readline().rstrip('\n')
+            value = file.readline().rstrip('\n')
+
+            if line[0] == 'M':
+                Vehicle1 = vehicle(value, "metro", line)
+                Vehicle2 = vehicle(value, "taxi", line)
+
+                self.graph[start][destination].append(Vehicle1)
+                self.graph[start][destination].append(Vehicle2)
+
+                self.graph[destination][start].append(Vehicle1)
+                self.graph[destination][start].append(Vehicle2)
+            
+            elif line[0] == "B":
+                Vehicle1 = vehicle(value, 'Bus', line)
+                
+                self.graph[start][destination].append(Vehicle1)
+                self.graph[destination][start].append(Vehicle1)
+
+
+
+tehran = Tehran()
+
+# d = {'A': 1, "B":2, "C":3}
+
+# print(*d.items())
+
+for i in tehran.graph:
+    print(i, end=" -> ")
+    for j in tehran.graph[i]:
+        print(j, end=": ")
+        for vec in tehran.graph[i][j]:
+            print(vec.get_type_of_vehicle(), end=' ')
+
+
+            
+            
+
+
+
--- vehicle.py
+++ vehicle.py
@@ -1,16 +1,18 @@
+import sys
+
 class vehicle:
-    def __init__(self, value:float, name:str, line:str) -> None:
+    def __init__(self, value:int = sys.maxsize, name:str = "", line:str = "") -> None:
         self.set_name_of_vehicle(name)
         self.set_line(line)
         self.set_value(value)
 
-    def set_value (self, value:float) -> None:
+    def set_value (self, value:int) -> None:
         self.value = value
     def set_name_of_vehicle(self, name:str) -> None:
         self.vehicle_name = name
     def set_line(self, line:str) -> None:
         self.line = line
-    def get_value(self) -> float:
+    def get_value(self) -> int:
         return self.value
     def get_line(self) -> str:
         return self.line
