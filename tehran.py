61d4ae0d76066676c82d661cb8128fef9209f666
arya237 <naderiarya20@gmail.com>
2024-03-24 15:42:53 +0330

    start

---
+++ tehran.py
@@ -0,0 +1,8 @@
+from collections import defaultdict as mymap
+from vehicle import vehicle
+
+# class Tehran:
+#     def __init__(self) -> None:
+#         self.graph = mymap(lambda: mymap(list))
+        
+#     def
---
+++ vehicle.py
@@ -0,0 +1,21 @@
+class vehicle:
+    def __init__(self, value:float, name:str, line:str) -> None:
+        self.set_name_of_vehicle(name)
+        self.set_line(line)
+        self.set_value(value)
+
+    def set_value (self, value:float) -> None:
+        self.value = value
+    def set_name_of_vehicle(self, name:str) -> None:
+        self.vehicle_name = name
+    def set_line(self, line:str) -> None:
+        self.line = line
+    def get_value(self) -> float:
+        return self.value
+    def get_line(self) -> str:
+        return self.line
+    def get_type_of_vehicle(self) -> str:
+        return self.vehicle_name
+
+
+
