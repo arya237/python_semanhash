b93f8136a4ea9df782909147c46a49c3564331c9
arya237 <naderiarya20@gmail.com>
2024-03-26 12:13:12 +0330

    completed path

--- tehran.py
+++ tehran.py
@@ -22,6 +22,7 @@ class Tehran:
 
     def __init__(self) -> None:
         self.graph = mymap(lambda: mymap(mylist))
+        self.lines = mymap(list[str])
         self.read_from_file()
         
 
@@ -29,12 +30,9 @@ class Tehran:
         min = float('inf')
         min_name = ""  
         for key, Value in list.items():
-            # print(key, ": " ,(key not in visited))
             if (key not in visited) and Value.value < min:
                 min = list[key].value
                 min_name = key
-        
-        print(min_name)
         return min_name
             
     def read_from_file(self) -> None:
@@ -87,40 +85,35 @@ class Tehran:
         for i in range(0, len(self.graph)):
             
             min_state = self.find_minimum(pathes, visited)
-            # print(min_state,": ",pathes[min_state].value)
-            # print(min_state)
             visited.append(min_state)
-            # print("visited: ",*visited)
 
             for key, value in self.graph[min_state].items():
-                # print("value: " ,value.get_min().value)
-                # print(key,": ",pathes[key].value)
                 if (key not in visited and int(pathes[min_state].value) != float('inf') 
                     and int(pathes[min_state].value) + int(value.get_min().value) < float(pathes[key].value)):
-                    # print("value: " ,pathes[min_state].value + value.get_min().value)
                     pathes[key].value = int(pathes[min_state].value) + int(value.get_min().value)
-                    # print(key, ': ', pathes[key].value)
+                    pathes[key].direction = pathes[min_state].direction.copy()
+                    pathes[key].direction.append(key)
+                    pathes[key].type_vehicle = pathes[min_state].type_vehicle.copy()
+                    pathes[key].type_vehicle.append(value.get_min().get_type_of_vehicle())
 
-            # for i,j in pathes.items():
-            #     print(i, ":", j.value)  
-        
-        # for i in pathes:
-        #     print(i, ":", pathes[i])
-        # print(pathes[destiny].value)
                     
-        print(pathes[destiny].value)
+        print(pathes[destiny].value, end=":\n")
+        print(pathes[destiny].direction[0], end=" -> ")
+
+        for i in range(1, len(pathes[destiny].direction)):
+            print(" ( " ,pathes[destiny].type_vehicle[i - 1], " ) ", pathes[destiny].direction[i], end=" -> ")
                     
                       
 
 
+
+
+
+
 tehran = Tehran()
-
-
-# print(tehran.graph["Emam Hossein"]["Haftom-e Tir"])
-# min_state = "Mirdamad"
-# for key, value in tehran.graph[min_state].items():
-#     print(key, value)
-tehran.find_shortest_path("Mirdamad", "Bimeh")
+src = input()
+destiny = input()
+tehran.find_shortest_path(src, destiny)
 
 # d = {'A': 1, "B":2, "C":3}
 
